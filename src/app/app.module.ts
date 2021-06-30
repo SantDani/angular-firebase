305fc3f3036a77438c245e199392a8a09bf52686
SantiUAB <santiagodaniel.rodriguez@e-campus.uab.cat>
2021-06-30 22:00:07 +0200

    Init project add list employee

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeeComponent,
+    CreateEmployeeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
