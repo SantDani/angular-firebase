dd8fccc50d893ccd55e1e8c68ec88f132be2098b
SantiDani <profsantirp@gmail.com>
2021-07-01 09:15:06 +0200

    ignore env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    production: true,
+    apiKey: "AIzaSyCafwCoXkD3THITHIklrEa13OgvhPRvUwA",
+    authDomain: "employee-e3dc3.firebaseapp.com",
+    projectId: "employee-e3dc3",
+    storageBucket: "employee-e3dc3.appspot.com",
+    messagingSenderId: "956825747434",
+    appId: "1:956825747434:web:71898ca8deeacadd69bc93",
+    measurementId: "G-F87GP84HQ5"
+  }
 };
 
 /*
