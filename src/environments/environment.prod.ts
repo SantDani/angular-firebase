0ba4f21e54a18b98d96588649a47bd3156dd9e73
SantiDani <profsantirp@gmail.com>
2021-07-04 15:36:10 +0200

    update for Host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+import{ environment as common} from "./environment";
+
 export const environment = {
-  production: true
+
+  production: true,
+  firebase:{
+    // production: true,
+    apiKey: "AIzaSyCafwCoXkD3THITHIklrEa13OgvhPRvUwA",
+    authDomain: "employee-e3dc3.firebaseapp.com",
+    projectId: "employee-e3dc3",
+    storageBucket: "employee-e3dc3.appspot.com",
+    messagingSenderId: "956825747434",
+    appId: "1:956825747434:web:71898ca8deeacadd69bc93",
+    measurementId: "G-F87GP84HQ5"
+  }
 };
